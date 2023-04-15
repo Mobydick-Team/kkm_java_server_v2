ed3dbe356a645e969d4af35907d97b4ac3ee8c82
soohyuneeee <soohyuneeee12@gmail.com>
2023-04-16 00:36:53 +0900

    {Fix} 어노테이션 정리

--- src/main/java/com/kkm/kkm_server_v2/domian/auth/presentation/dto/request/LoginRequest.java
+++ src/main/java/com/kkm/kkm_server_v2/domian/auth/presentation/dto/request/LoginRequest.java
@@ -2,13 +2,15 @@ package com.kkm.kkm_server_v2.domian.auth.presentation.dto.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     @NotNull
-    private Long userId;
+    private String userId;
 
 }
