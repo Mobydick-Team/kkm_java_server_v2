d9babb3544f8ebadb8bcbeb23c26ab728aff4603
soohyuneeee <soohyuneeee12@gmail.com>
2023-04-16 02:33:25 +0900

    {Fix} Jpa -> Crud

--- src/main/java/com/kkm/kkm_server_v2/domian/user/domain/repository/UserRepository.java
+++ src/main/java/com/kkm/kkm_server_v2/domian/user/domain/repository/UserRepository.java
@@ -1,16 +1,17 @@
 package com.kkm.kkm_server_v2.domian.user.domain.repository;
 
 import com.kkm.kkm_server_v2.domian.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
 
     boolean existsUserByNickname(String nickname);
 
     boolean existsUserByUserId(String userId);
 
     Optional<User> findByUserId(String userId);
+
     Optional<User> findByNickname(String nickname);
 }
