c5e8d15c1b9dde622da0f3d559501d5c9edf147e
yongbin1 <rhdydqls1024@naver.com>
2023-04-16 21:29:14 +0900

    Add - Post Entity

--- src/main/java/com/kkm/kkm_server_v2/domain/post/domain/Post.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/domain/Post.java
@@ -1,9 +1,12 @@
 package com.kkm.kkm_server_v2.domain.post.domain;
 
+import com.kkm.kkm_server_v2.domain.post.domain.enums.PostCategory;
 import com.kkm.kkm_server_v2.global.entity.BaseTime;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
 
 import javax.persistence.*;
 
@@ -16,4 +19,32 @@ public class Post extends BaseTime {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
 
+    private String title;
+
+    private String content;
+
+    @ColumnDefault("0")
+    private int price;
+
+    private String process; // 거래 방법 추후 변경 예정
+
+    @Enumerated(EnumType.STRING)
+    private PostCategory category;
+
+    public void updatePost(String title, String content, int price, String process, PostCategory category) {
+        this.title = title;
+        this.content = content;
+        this.price = price;
+        this.process = process;
+        this.category = category;
+    }
+
+    @Builder
+    public Post(String title, String content, int price, String process, PostCategory category) {
+        this.title = title;
+        this.content = content;
+        this.price = price;
+        this.process = process;
+        this.category = category;
+    }
 }
