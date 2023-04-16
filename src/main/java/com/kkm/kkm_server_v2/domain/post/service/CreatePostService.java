4c14b0ce03c7f35b2747fffeb743b67f4244447e
yongbin1 <rhdydqls1024@naver.com>
2023-04-16 21:51:37 +0900

    Feat - CreatePostService 로직 작성(추후 이미지 로직 추가 예정)

--- src/main/java/com/kkm/kkm_server_v2/domain/post/service/CreatePostService.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/service/CreatePostService.java
@@ -1,4 +1,29 @@
 package com.kkm.kkm_server_v2.domain.post.service;
 
+import com.kkm.kkm_server_v2.domain.post.domain.Post;
+import com.kkm.kkm_server_v2.domain.post.domain.repository.PostRepository;
+import com.kkm.kkm_server_v2.domain.post.presentation.dto.request.CreatePostRequest;
+import com.kkm.kkm_server_v2.domain.user.domain.User;
+import com.kkm.kkm_server_v2.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class CreatePostService {
+
+    private final UserFacade userFacade;
+    private final PostRepository postRepository;
+
+    @Transactional
+    public void execute(CreatePostRequest request) {
+        User user = userFacade.getCurrentUser();
+
+        Post post = request.toEntity();
+        post.setAuthor(user);
+
+        postRepository.save(post);
+    }
+
 }
