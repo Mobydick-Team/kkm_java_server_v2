7580bf556d306c17dbb0ed473949e7cbfb3be948
yongbin1 <rhdydqls1024@naver.com>
2023-04-16 23:19:17 +0900

    Feat - UpdatePostService 로직 작성 (추후 이미지 변경 로직 추가 예정)

--- src/main/java/com/kkm/kkm_server_v2/domain/post/service/UpdatePostService.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/service/UpdatePostService.java
@@ -1,2 +1,27 @@
-package com.kkm.kkm_server_v2.domain.post.service;public class UpdatePostService {
+package com.kkm.kkm_server_v2.domain.post.service;
+
+import com.kkm.kkm_server_v2.domain.post.domain.Post;
+import com.kkm.kkm_server_v2.domain.post.domain.repository.PostRepository;
+import com.kkm.kkm_server_v2.domain.post.exception.PostNotFoundException;
+import com.kkm.kkm_server_v2.domain.post.presentation.dto.request.UpdatePostRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UpdatePostService {
+
+    private final PostRepository postRepository;
+
+    @Transactional
+    public void execute(Long postId, UpdatePostRequest request) {
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> PostNotFoundException.EXCEPTION);
+
+        post.updatePost(request.getTitle(), request.getContent(), request.getPrice(),
+                request.getProcess(), request.getCategory());
+
+        postRepository.save(post);
+    }
 }
