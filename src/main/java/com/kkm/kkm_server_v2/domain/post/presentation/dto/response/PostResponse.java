4b441538279070c818db9cec381f2f4b59a1e75c
yongbin1 <rhdydqls1024@naver.com>
2023-04-17 00:37:32 +0900

    Feat - Entity -> Response 변환해주는 메서드 작성

--- src/main/java/com/kkm/kkm_server_v2/domain/post/presentation/dto/response/PostResponse.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/presentation/dto/response/PostResponse.java
@@ -1,5 +1,6 @@
 package com.kkm.kkm_server_v2.domain.post.presentation.dto.response;
 
+import com.kkm.kkm_server_v2.domain.post.domain.Post;
 import com.kkm.kkm_server_v2.domain.post.domain.enums.PostCategory;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -15,5 +16,16 @@ public class PostResponse {
     private int price;
     private String process;
     private PostCategory category;
+
+    public static PostResponse of(Post post) {
+        return PostResponse.builder()
+                .postId(post.getPostId())
+                .title(post.getTitle())
+                .content(post.getContent())
+                .price(post.getPrice())
+                .process(post.getProcess())
+                .category(post.getCategory())
+                .build();
+    }
 
 }
