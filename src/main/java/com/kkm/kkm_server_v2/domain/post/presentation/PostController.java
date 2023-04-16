11f0d1d21b923bb358dcf4e9c0beabd1df60dcbd
yongbin1 <rhdydqls1024@naver.com>
2023-04-16 21:52:55 +0900

    Feat - CreatePost Controller 추가

--- src/main/java/com/kkm/kkm_server_v2/domain/post/presentation/PostController.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/presentation/PostController.java
@@ -1,4 +1,24 @@
 package com.kkm.kkm_server_v2.domain.post.presentation;
 
+import com.kkm.kkm_server_v2.domain.post.presentation.dto.request.CreatePostRequest;
+import com.kkm.kkm_server_v2.domain.post.service.CreatePostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/post")
+@RequiredArgsConstructor
 public class PostController {
+
+    private final CreatePostService createPostService;
+
+    @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createPost(
+            @RequestBody CreatePostRequest request
+    ) {
+        createPostService.execute(request);
+    }
+
 }
