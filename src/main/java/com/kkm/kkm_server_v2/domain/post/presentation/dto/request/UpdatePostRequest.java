03378ffcd6bdfc534c7538975b0fdfc9f4a2cbc3
yongbin1 <rhdydqls1024@naver.com>
2023-04-16 22:35:34 +0900

    Add - UpdatePostRequest

---
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/presentation/dto/request/UpdatePostRequest.java
@@ -0,0 +1,18 @@
+package com.kkm.kkm_server_v2.domain.post.presentation.dto.request;
+
+import com.kkm.kkm_server_v2.domain.post.domain.enums.PostCategory;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UpdatePostRequest {
+
+    private String title;
+    private String content;
+    private int price;
+    private String process;
+    private PostCategory category;
+
+}
