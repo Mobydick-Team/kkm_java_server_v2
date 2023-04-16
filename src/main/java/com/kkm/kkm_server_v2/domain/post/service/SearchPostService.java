42b46ed29105b63725bfa831862a305a97dd50a7
yongbin1 <rhdydqls1024@naver.com>
2023-04-17 00:38:17 +0900

    Feat - SearchPostService 로직 작성

--- src/main/java/com/kkm/kkm_server_v2/domain/post/service/SearchPostService.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/service/SearchPostService.java
@@ -1,2 +1,23 @@
-package com.kkm.kkm_server_v2.domain.post.service;public class SearchPostService {
+package com.kkm.kkm_server_v2.domain.post.service;
+
+import com.kkm.kkm_server_v2.domain.post.domain.Post;
+import com.kkm.kkm_server_v2.domain.post.domain.repository.PostRepository;
+import com.kkm.kkm_server_v2.domain.post.exception.PostNotFoundException;
+import com.kkm.kkm_server_v2.domain.post.presentation.dto.response.PostResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class SearchPostService {
+
+    private final PostRepository postRepository;
+
+    @Transactional(readOnly = true)
+    public PostResponse execute(Long postId) {
+        return PostResponse.of(postRepository.findById(postId)
+                .orElseThrow(() -> PostNotFoundException.EXCEPTION));
+    }
+
 }
