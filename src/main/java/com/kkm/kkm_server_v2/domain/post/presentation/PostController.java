59bb04bd72e42b49261284ece0251a7b41d36222
yongbin1 <rhdydqls1024@naver.com>
2023-04-17 00:39:47 +0900

    Feat - SearchPost Controller 코드 작성

--- src/main/java/com/kkm/kkm_server_v2/domain/post/presentation/PostController.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/presentation/PostController.java
@@ -2,8 +2,10 @@ package com.kkm.kkm_server_v2.domain.post.presentation;
 
 import com.kkm.kkm_server_v2.domain.post.presentation.dto.request.CreatePostRequest;
 import com.kkm.kkm_server_v2.domain.post.presentation.dto.request.UpdatePostRequest;
+import com.kkm.kkm_server_v2.domain.post.presentation.dto.response.PostResponse;
 import com.kkm.kkm_server_v2.domain.post.service.CreatePostService;
 import com.kkm.kkm_server_v2.domain.post.service.DeletePostService;
+import com.kkm.kkm_server_v2.domain.post.service.SearchPostService;
 import com.kkm.kkm_server_v2.domain.post.service.UpdatePostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,6 +19,7 @@ public class PostController {
     private final CreatePostService createPostService;
     private final UpdatePostService updatePostService;
     private final DeletePostService deletePostService;
+    private final SearchPostService searchPostService;
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
@@ -41,5 +44,13 @@ public class PostController {
     ) {
         deletePostService.execute(id);
     }
+
+    @GetMapping("/{id}")
+    public PostResponse getPost(
+            @PathVariable("id") Long id
+    ) {
+        return searchPostService.execute(id);
+    }
+
 
 }
