53675d1b2efa2dedd3cb9058a1e166939b4ce329
yongbin1 <rhdydqls1024@naver.com>
2023-04-17 00:05:58 +0900

    Feat - UpdatePost Controller 코드 작성

--- src/main/java/com/kkm/kkm_server_v2/domain/post/presentation/PostController.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/presentation/PostController.java
@@ -1,7 +1,9 @@
 package com.kkm.kkm_server_v2.domain.post.presentation;
 
 import com.kkm.kkm_server_v2.domain.post.presentation.dto.request.CreatePostRequest;
+import com.kkm.kkm_server_v2.domain.post.presentation.dto.request.UpdatePostRequest;
 import com.kkm.kkm_server_v2.domain.post.service.CreatePostService;
+import com.kkm.kkm_server_v2.domain.post.service.UpdatePostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 public class PostController {
 
     private final CreatePostService createPostService;
+    private final UpdatePostService updatePostService;
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
@@ -19,6 +22,14 @@ public class PostController {
             @RequestBody CreatePostRequest request
     ) {
         createPostService.execute(request);
+    }
+
+    @PatchMapping("/{id}")
+    public void updatePost(
+            @PathVariable("id") Long id,
+            @RequestBody UpdatePostRequest request
+    ) {
+        updatePostService.execute(id, request);
     }
 
 }
