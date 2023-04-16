f234c705a939de1626b2c75a226c2403e614d1fc
yongbin1 <rhdydqls1024@naver.com>
2023-04-17 00:05:13 +0900

    Add - PostNotFoundException

--- src/main/java/com/kkm/kkm_server_v2/domain/post/exception/PostNotFoundException.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/exception/PostNotFoundException.java
@@ -1,2 +1,13 @@
-package com.kkm.kkm_server_v2.domain.post.exception;public class PostNotFoundException {
+package com.kkm.kkm_server_v2.domain.post.exception;
+
+import com.kkm.kkm_server_v2.domain.post.exception.error.PostErrorProperty;
+import com.kkm.kkm_server_v2.global.error.exception.KkmException;
+
+public class PostNotFoundException extends KkmException {
+
+    public static final PostNotFoundException EXCEPTION = new PostNotFoundException();
+
+    private PostNotFoundException() {
+        super(PostErrorProperty.POST_NOT_FOUND);
+    }
 }
