b79ce322e3d0bce21e5c08630aa5d4eff6c0c10c
yongbin1 <rhdydqls1024@naver.com>
2023-04-17 00:24:24 +0900

    Feat - DeletePostService 로직 작성

--- src/main/java/com/kkm/kkm_server_v2/domain/post/service/DeletePostService.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/service/DeletePostService.java
@@ -1,2 +1,32 @@
-package com.kkm.kkm_server_v2.domain.post.service;public class DeletePostService {
+package com.kkm.kkm_server_v2.domain.post.service;
+
+import com.kkm.kkm_server_v2.domain.post.domain.Post;
+import com.kkm.kkm_server_v2.domain.post.domain.repository.PostRepository;
+import com.kkm.kkm_server_v2.domain.post.exception.PostAccessWrongException;
+import com.kkm.kkm_server_v2.domain.post.exception.PostNotFoundException;
+import com.kkm.kkm_server_v2.domain.user.domain.User;
+import com.kkm.kkm_server_v2.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class DeletePostService {
+
+    private final UserFacade userFacade;
+    private final PostRepository postRepository;
+
+    @Transactional
+    public void execute(Long postId) {
+        User user = userFacade.getCurrentUser();
+        Post post = postRepository.findById(postId)
+                .orElseThrow(() -> PostNotFoundException.EXCEPTION);
+
+        if(post.getAuthor().equals(user))
+            throw PostAccessWrongException.EXCEPTION;
+
+        postRepository.delete(post);
+    }
+
 }
