095bb80ff60ca7d9ac4e94107a6acf35c785277c
yongbin1 <rhdydqls1024@naver.com>
2023-04-17 01:21:20 +0900

    Feat - Category 별로 조회하는 repository 코드 작성

--- src/main/java/com/kkm/kkm_server_v2/domain/post/domain/repository/PostRepository.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/domain/repository/PostRepository.java
@@ -1,7 +1,13 @@
 package com.kkm.kkm_server_v2.domain.post.domain.repository;
 
 import com.kkm.kkm_server_v2.domain.post.domain.Post;
+import com.kkm.kkm_server_v2.domain.post.domain.enums.PostCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    Page<Post> findByCategory(PostCategory category, Pageable pageable);
+
 }
