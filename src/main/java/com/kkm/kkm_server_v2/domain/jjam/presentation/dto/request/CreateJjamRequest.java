27aabbf3f2b89894b8dc18c8fa9ac52ce635bb4e
soohyuneeee <soohyuneeee12@gmail.com>
2023-04-17 17:35:19 +0900

    {Add} CreateJjamRequest

---
+++ src/main/java/com/kkm/kkm_server_v2/domain/jjam/presentation/dto/request/CreateJjamRequest.java
@@ -0,0 +1,10 @@
+package com.kkm.kkm_server_v2.domain.jjam.presentation.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CreateJjamRequest {
+}
