429aeab46d44e459e6b22f0474626b750748341d
yongbin1 <rhdydqls1024@naver.com>
2023-04-17 09:23:47 +0900

    Feat - ImageUpload 서비스 로직 작성 및 Create에서 Image 추가하는 로직 추가

--- src/main/java/com/kkm/kkm_server_v2/domain/post/service/CreatePostService.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/service/CreatePostService.java
@@ -1,13 +1,20 @@
 package com.kkm.kkm_server_v2.domain.post.service;
 
+import com.kkm.kkm_server_v2.domain.post.domain.Image;
 import com.kkm.kkm_server_v2.domain.post.domain.Post;
+import com.kkm.kkm_server_v2.domain.post.domain.repository.ImageRepository;
 import com.kkm.kkm_server_v2.domain.post.domain.repository.PostRepository;
+import com.kkm.kkm_server_v2.domain.post.exception.ImageNotFoundException;
 import com.kkm.kkm_server_v2.domain.post.presentation.dto.request.CreatePostRequest;
 import com.kkm.kkm_server_v2.domain.user.domain.User;
 import com.kkm.kkm_server_v2.domain.user.facade.UserFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,14 +22,23 @@ public class CreatePostService {
 
     private final UserFacade userFacade;
     private final PostRepository postRepository;
+    private final ImageRepository imageRepository;
 
     @Transactional
     public void execute(CreatePostRequest request) {
         User user = userFacade.getCurrentUser();
 
         Post post = request.toEntity();
-        post.setAuthor(user);
+        if(!request.getUrls().isEmpty()) {
+            List<Image> images = request.getUrls().stream().map(url ->
+                imageRepository.findByUrl(url)
+                        .orElseThrow(() -> ImageNotFoundException.EXCEPTION)
+            ).peek(image -> image.setPost(post)).collect(Collectors.toList());
+            post.addImage(images);
+        }
 
+        post.setAuthor(user);
+        user.addPost(post);
         postRepository.save(post);
     }
 
--- src/main/java/com/kkm/kkm_server_v2/domain/post/service/UploadImageService.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/service/UploadImageService.java
@@ -1,2 +1,35 @@
-package com.kkm.kkm_server_v2.domain.post.service;public class UploadImageService {
+package com.kkm.kkm_server_v2.domain.post.service;
+
+import com.kkm.kkm_server_v2.domain.post.domain.Image;
+import com.kkm.kkm_server_v2.domain.post.domain.repository.ImageRepository;
+import com.kkm.kkm_server_v2.domain.post.presentation.dto.response.ImageResponse;
+import com.kkm.kkm_server_v2.global.infra.S3.service.AwsS3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class UploadImageService {
+
+    private final ImageRepository imageRepository;
+    private final AwsS3Service awsS3Service;
+
+    @Transactional
+    public ImageResponse execute(List<MultipartFile> files) {
+        List<String> urls = awsS3Service.uploadFile(files);
+
+        List<Image> images = urls.stream().map(Image::new)
+                .collect(Collectors.toList());
+        imageRepository.saveAll(images);
+
+        return ImageResponse.builder()
+                .urls(urls)
+                .build();
+    }
+
 }
