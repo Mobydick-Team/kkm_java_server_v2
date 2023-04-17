915498ab5295ddcdccb59e3202fc34748a7a318d
soohyuneeee <soohyuneeee12@gmail.com>
2023-04-17 17:45:20 +0900

    {Add} JjamRepository

---
+++ src/main/java/com/kkm/kkm_server_v2/domain/jjam/domain/repository/JjamRepository.java
@@ -0,0 +1,7 @@
+package com.kkm.kkm_server_v2.domain.jjam.domain.repository;
+
+import com.kkm.kkm_server_v2.domain.jjam.domain.Jjam;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JjamRepository extends JpaRepository<Jjam,Long> {
+}
