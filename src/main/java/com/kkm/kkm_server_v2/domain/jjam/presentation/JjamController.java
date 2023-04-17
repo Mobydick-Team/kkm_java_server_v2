f0352e0867070252b00a76df4d3202321ab20554
soohyuneeee <soohyuneeee12@gmail.com>
2023-04-17 17:25:00 +0900

    {Add} JJamController

---
+++ src/main/java/com/kkm/kkm_server_v2/domain/jjam/presentation/JjamController.java
@@ -0,0 +1,11 @@
+package com.kkm.kkm_server_v2.domain.jjam.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jjam")
+@RequiredArgsConstructor
+public class JjamController {
+}
