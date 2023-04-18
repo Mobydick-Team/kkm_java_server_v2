4a01cad18582c407ef12da54bab593d235f4f088
soohyuneeee <soohyuneeee12@gmail.com>
2023-04-18 09:53:58 +0900

    {Add} JjamNotFoundException

--- src/main/java/com/kkm/kkm_server_v2/domain/jjam/exception/error/JjamErrorProperty.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/jjam/exception/error/JjamErrorProperty.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum JjamErrorProperty implements ErrorProperty {
-    JJAM_NOT_FOUND(HttpStatus.NOT_FOUND, "쨈을 찾을 수 없습니다.");
+    JJAM_NOT_FOUND(HttpStatus.NOT_FOUND, "쨈을 찾을 수 없습니다."),
+    JJAM_ACCESS_WRONG(HttpStatus.FORBIDDEN,"쨈 접근권한이 없습니다.");
 
     private final HttpStatus status;
     private final String message;
