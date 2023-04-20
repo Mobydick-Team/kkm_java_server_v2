b8d0d1536364f2afb4a2e3fa68272fc7b50a4e63
yongbin1 <rhdydqls1024@naver.com>
2023-04-21 00:34:47 +0900

    Fix - @Setter 어노테이션 제거 및 @RefreshScope 어노테이션 추가

--- src/main/java/com/kkm/kkm_server_v2/global/config/properties/JwtProperties.java
+++ src/main/java/com/kkm/kkm_server_v2/global/config/properties/JwtProperties.java
@@ -1,14 +1,16 @@
 package com.kkm.kkm_server_v2.global.config.properties;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
-@Setter
 @Getter
+@AllArgsConstructor
 @ConstructorBinding
 @ConfigurationProperties("jwt")
+@RefreshScope
 public class JwtProperties {
 
     private Long accessExp;
