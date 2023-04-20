9b0c343983147ff6837ad5a8c7131a88068c4900
soohyuneeee <soohyuneeee12@gmail.com>
2023-04-21 00:19:39 +0900

    {Add} TradeController

---
+++ src/main/java/com/kkm/kkm_server_v2/domain/trade/presentation/TradeController.java
@@ -0,0 +1,11 @@
+package com.kkm.kkm_server_v2.domain.trade.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/trade")
+@RequiredArgsConstructor
+public class TradeController {
+}
