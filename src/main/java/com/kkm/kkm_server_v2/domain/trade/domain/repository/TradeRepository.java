db45b9bd3a3a03a4e5372bf6e920127cb4370473
soohyuneeee <soohyuneeee12@gmail.com>
2023-04-24 12:10:31 +0900

    {Feat} createTrade

--- src/main/java/com/kkm/kkm_server_v2/domain/trade/domain/repository/TradeRepository.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/trade/domain/repository/TradeRepository.java
@@ -1,7 +1,13 @@
 package com.kkm.kkm_server_v2.domain.trade.domain.repository;
 
+import com.kkm.kkm_server_v2.domain.post.domain.Post;
 import com.kkm.kkm_server_v2.domain.trade.domain.Trade;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TradeRepository extends JpaRepository<Trade, Long> {
+    @Query("select t from Trade t where t.postId = :post order by t.tradeTime desc")
+    List<Trade> findAllByPostIdOrderByTradeTime(Post post);
 }
