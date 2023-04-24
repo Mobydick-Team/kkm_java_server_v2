10b9c7bdcaf82b0f3f825d5bc04549058be5d669
soohyuneeee <soohyuneeee12@gmail.com>
2023-04-24 17:24:34 +0900

    {Add} Review

--- src/main/java/com/kkm/kkm_server_v2/domain/review/domain/Review.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/review/domain/Review.java
@@ -1,4 +1,37 @@
 package com.kkm.kkm_server_v2.domain.review.domain;
 
+import com.kkm.kkm_server_v2.domain.review.domain.enums.RecieveKkomak;
+import com.kkm.kkm_server_v2.domain.trade.domain.Trade;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_review")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private RecieveKkomak kkomak;
+    private String content;
+    @OneToOne
+    @JoinColumn(name = "fk_trade")
+    private Trade source;
+    @Builder
+    public Review(RecieveKkomak kkomak, String content, Trade source) {
+        this.kkomak = kkomak;
+        this.content = content;
+        this.source = source;
+    }
 }
