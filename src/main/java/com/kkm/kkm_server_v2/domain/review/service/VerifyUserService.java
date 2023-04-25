b9864364996b62258779684394fccc17161e6d93
soohyuneeee <soohyuneeee12@gmail.com>
2023-04-25 09:56:53 +0900

    {Feat} - VerifyUserService

---
+++ src/main/java/com/kkm/kkm_server_v2/domain/review/service/VerifyUserService.java
@@ -0,0 +1,30 @@
+package com.kkm.kkm_server_v2.domain.review.service;
+
+import com.kkm.kkm_server_v2.domain.post.domain.Post;
+import com.kkm.kkm_server_v2.domain.post.domain.repository.PostRepository;
+import com.kkm.kkm_server_v2.domain.post.exception.PostNotFoundException;
+import com.kkm.kkm_server_v2.domain.review.exception.ReviewAccessWrongException;
+import com.kkm.kkm_server_v2.domain.trade.domain.Trade;
+import com.kkm.kkm_server_v2.domain.trade.domain.repository.TradeRepository;
+import com.kkm.kkm_server_v2.domain.trade.exception.TradeNotCompletedException;
+import com.kkm.kkm_server_v2.domain.user.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+@Service
+@RequiredArgsConstructor
+public class VerifyUserService {
+    private final PostRepository postRepository;
+    private final TradeRepository tradeRepository;
+
+    public Trade execute(User user, Long sourceId) {
+        Post post = postRepository.findById(sourceId)
+                .orElseThrow(() -> PostNotFoundException.EXCEPTION);
+        Trade trade = tradeRepository.findByPostIdAndReceiverId(post, user.getId());
+        if (trade == null) throw ReviewAccessWrongException.EXCEPTION;
+        if (trade.getTradeTime().isBefore(LocalDateTime.now())) throw TradeNotCompletedException.EXCEPTION;
+        return trade;
+    }
+}
