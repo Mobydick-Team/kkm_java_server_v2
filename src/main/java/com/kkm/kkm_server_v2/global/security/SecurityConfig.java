d490d4091c9fb2408b39d839e5e2183e96468746
yongbin1 <rhdydqls1024@naver.com>
2023-04-27 00:20:47 +0900

    Add - Security Config 추가

---
+++ src/main/java/com/kkm/kkm_server_v2/global/security/SecurityConfig.java
@@ -0,0 +1,70 @@
+package com.kkm.kkm_server_v2.global.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kkm.kkm_server_v2.global.error.filter.GlobalErrorFilter;
+import com.kkm.kkm_server_v2.global.security.auth.AuthDetailsService;
+import com.kkm.kkm_server_v2.global.security.jwt.JwtTokenProvider;
+import com.kkm.kkm_server_v2.global.security.jwt.JwtValidateService;
+import com.kkm.kkm_server_v2.global.security.jwt.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final AuthDetailsService authDetailsService;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtValidateService jwtValidateService;
+    private final ObjectMapper mapper;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web ->
+                web
+                        .ignoring()
+                        .mvcMatchers("/swagger-ui/**", "/configuration/**", "/swagger-resources/**", "/v3/api-docs", "/webjars/**", "/webjars/springfox-swagger-ui/*.{js,css}");
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http
+                .httpBasic().disable()
+                .csrf().disable()
+                .formLogin().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .antMatchers(HttpMethod.POST, "/user/signup").permitAll()
+                .antMatchers(HttpMethod.GET, "/user/check/*").permitAll()
+                .antMatchers(HttpMethod.GET, "/auth/kakao/info").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth/kakao/login").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .addFilterBefore(new JwtAuthenticationFilter(authDetailsService, jwtTokenProvider, jwtValidateService),
+                        UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new GlobalErrorFilter(mapper), JwtAuthenticationFilter.class);
+
+        return http.build();
+    }
+}
