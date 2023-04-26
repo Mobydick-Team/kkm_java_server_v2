de34befb3a4e48d9165e0e9f87a05618fbf634d0
yongbin1 <rhdydqls1024@naver.com>
2023-04-27 00:25:13 +0900

    Fix - Swagger Info 변경

--- src/main/java/com/kkm/kkm_server_v2/global/config/SwaggerConfig.java
+++ src/main/java/com/kkm/kkm_server_v2/global/config/SwaggerConfig.java
@@ -1,9 +1,7 @@
 package com.kkm.kkm_server_v2.global.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,17 +11,11 @@ public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
         Info info = new Info()
-                .title("다사랑 API")
+                .title("꼬꼬막 API")
                 .version("version 1.0")
-                .description("다사랑 API 문서");
+                .description("꼬꼬막 API 문서");
 
         return new OpenAPI()
-                .components(new Components()
-                        .addSecuritySchemes(
-                                "bearer-token",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer").bearerFormat("JWT")))
                 .info(info);
     }
 
