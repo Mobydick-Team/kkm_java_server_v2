ebdfd7216fb7c1f4ff410eac59326e47fc2311d0
soohyuneeee <soohyuneeee12@gmail.com>
2023-04-27 13:27:55 +0900

    {Add} @Enumrated(EnumType.STRING)

--- src/main/java/com/kkm/kkm_server_v2/domain/review/domain/Review.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/review/domain/Review.java
@@ -9,6 +9,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -25,6 +27,7 @@ public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Enumerated(EnumType.STRING)
     private RecieveKkomak kkm;
     private String content;
     @OneToOne
