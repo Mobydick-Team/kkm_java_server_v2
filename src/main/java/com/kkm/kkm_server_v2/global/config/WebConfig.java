d1532cc218cf2f51997ca771df588d378476bae5
yongbin1 <rhdydqls1024@naver.com>
2023-04-28 09:54:31 +0900

    WebConfig Swggaer 설정 삭제

--- src/main/java/com/kkm/kkm_server_v2/global/config/WebConfig.java
+++ src/main/java/com/kkm/kkm_server_v2/global/config/WebConfig.java
@@ -9,15 +9,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html")
-                .addResourceLocations("classpath:/META-INF/resources/");
-
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-
-    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
