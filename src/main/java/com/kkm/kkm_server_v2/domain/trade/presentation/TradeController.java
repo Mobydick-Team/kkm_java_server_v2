2fa7fe6949c30a8331c441e4a657ce89a947ddf9
soohyuneeee <soohyuneeee12@gmail.com>
2023-05-14 03:51:34 +0900

    {Feat} 거래종료 서비스

--- src/main/java/com/kkm/kkm_server_v2/domain/trade/presentation/TradeController.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/trade/presentation/TradeController.java
@@ -2,8 +2,10 @@ package com.kkm.kkm_server_v2.domain.trade.presentation;
 
 import com.kkm.kkm_server_v2.domain.trade.presentation.dto.request.CreateTradeRequest;
 import com.kkm.kkm_server_v2.domain.trade.service.CreateTradeService;
+import com.kkm.kkm_server_v2.domain.trade.service.EndTradeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class TradeController {
     private final CreateTradeService createTradeService;
+    private final EndTradeService endTradeService;
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
@@ -22,5 +25,12 @@ public class TradeController {
             @RequestBody CreateTradeRequest request
     ) {
         createTradeService.execute(request);
+    }
+
+    @PostMapping("/end/{tradeId}")
+    public void endTrade(
+            @PathVariable Long tradeId
+    ) {
+        endTradeService.execute(tradeId);
     }
 }
