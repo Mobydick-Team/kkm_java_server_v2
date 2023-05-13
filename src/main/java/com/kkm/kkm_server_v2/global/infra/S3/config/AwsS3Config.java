c525c859c433a7c6a77216694cbb5596233a496d
yongbin1 <rhdydqls1024@naver.com>
2023-05-14 03:43:36 +0900

    log 찍기

--- src/main/java/com/kkm/kkm_server_v2/global/infra/S3/config/AwsS3Config.java
+++ src/main/java/com/kkm/kkm_server_v2/global/infra/S3/config/AwsS3Config.java
@@ -5,9 +5,11 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@Slf4j
 @Configuration
 @RequiredArgsConstructor
 public class AwsS3Config {
@@ -16,6 +18,8 @@ public class AwsS3Config {
 
     @Bean
     public AmazonS3Client amazonS3Client() {
+        log.info("access : " + awsProperties.getAccessKey());
+        log.info("refresh : " + awsProperties.getSecretKey());
         BasicAWSCredentials awsCredentials = new BasicAWSCredentials(awsProperties.getAccessKey(), awsProperties.getSecretKey());
         return (AmazonS3Client) AmazonS3ClientBuilder.standard()
                 .withRegion(awsProperties.getRegion())
