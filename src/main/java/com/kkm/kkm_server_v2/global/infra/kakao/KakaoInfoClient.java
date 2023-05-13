48dd02968bc5a566698b86ad017a5b56a355830c
yongbin1 <rhdydqls1024@naver.com>
2023-05-14 02:25:28 +0900

    되는 줄

--- src/main/java/com/kkm/kkm_server_v2/global/infra/kakao/KakaoInfoClient.java
+++ src/main/java/com/kkm/kkm_server_v2/global/infra/kakao/KakaoInfoClient.java
@@ -11,7 +11,7 @@ public interface KakaoInfoClient {
 
     @PostMapping("/v2/user/me")
     @JsonProperty("id")
-    Long getProfile(
+    String getProfile(
             @RequestHeader("Authorization") String accessToken
     );
 
