52e3925c870abb93619345b6ef55cc3b20e9f968
soohyuneeee <soohyuneeee12@gmail.com>
2023-05-16 01:17:15 +0900

    {Add} Transactional

--- src/main/java/com/kkm/kkm_server_v2/domain/jjam/service/IsJjammedService.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/jjam/service/IsJjammedService.java
@@ -6,14 +6,18 @@ import com.kkm.kkm_server_v2.domain.post.domain.Post;
 import com.kkm.kkm_server_v2.domain.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class IsJjammedService {
     private final JjamRepository jjamRepository;
 
+    @Transactional
     public boolean execute(User user, Post post) {
-        Jjam jjam = jjamRepository.findByAgentAndPost(user, post);
-        return jjam != null;
+        Optional<Jjam> jjam = jjamRepository.findByAgentAndPost(user, post);
+        return jjam.isPresent();
     }
 }
