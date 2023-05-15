4e46b014832f648b9a577e4bb998e9acadb058e6
soohyuneeee <soohyuneeee12@gmail.com>
2023-05-16 01:16:15 +0900

    {Fix} Optional 추가해주면서 null 확인하는 로직 제거

--- src/main/java/com/kkm/kkm_server_v2/domain/jjam/domain/repository/JjamRepository.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/jjam/domain/repository/JjamRepository.java
@@ -7,11 +7,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JjamRepository extends JpaRepository<Jjam, Long> {
     List<Jjam> findAllByPost(Post post);
 
     @Query("SELECT j.post FROM Jjam j WHERE j.agent = :user")
     List<Post> findAllByAgent(User user);
-    Jjam findByAgentAndPost(User user, Post post);
+    Optional<Jjam> findByAgentAndPost(User user, Post post);
 }
