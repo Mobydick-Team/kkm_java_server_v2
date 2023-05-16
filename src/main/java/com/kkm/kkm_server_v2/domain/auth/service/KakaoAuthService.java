41838f92c8a823f4d10dc686e72fc95d6be52922
soohyuneeee <soohyuneeee12@gmail.com>
2023-05-16 10:03:42 +0900

    {Fix} 카카오 회원가입 확인

--- src/main/java/com/kkm/kkm_server_v2/domain/auth/service/KakaoAuthService.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/auth/service/KakaoAuthService.java
@@ -1,24 +1,25 @@
 package com.kkm.kkm_server_v2.domain.auth.service;
 
 import com.kkm.kkm_server_v2.domain.auth.presentation.dto.response.KakaoUserInfoResponse;
+import com.kkm.kkm_server_v2.domain.auth.presentation.dto.response.UserInfoResponse;
+import com.kkm.kkm_server_v2.domain.user.domain.repository.UserRepository;
 import com.kkm.kkm_server_v2.global.infra.kakao.KakaoInfo;
 import com.kkm.kkm_server_v2.global.infra.kakao.KakaoInfoClient;
 import com.kkm.kkm_server_v2.global.infra.kakao.KakaoLoginClient;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 @RequiredArgsConstructor
 public class KakaoAuthService {
     private final KakaoInfo kakaoInfo;
     private final KakaoLoginClient kakaoLoginClient;
     private final KakaoInfoClient kakaoInfoClient;
+    private final UserRepository userRepository;
 
-    public KakaoUserInfoResponse getKakaoProfile(String code) {
+    public UserInfoResponse getKakaoProfile(String code) {
         String response = kakaoLoginClient.getAccessToken(
                 "authorization_code",
                 kakaoInfo.getClientId(),
@@ -34,7 +35,18 @@ public class KakaoAuthService {
             String profile = new JSONObject(kakaoInfoClient.getProfile("Bearer " + accessToken))
                     .get("id")
                     .toString();
-            return new KakaoUserInfoResponse(Long.valueOf(profile));
+            KakaoUserInfoResponse kakaoUserInfoResponse = KakaoUserInfoResponse.builder()
+                    .id(Long.valueOf(profile))
+                    .build();
+            if (userRepository.findByUserId(profile).isPresent()) {
+                return UserInfoResponse.builder()
+                        .kakaoUserInfoResponse(kakaoUserInfoResponse)
+                        .isSignedUp(true)
+                        .build();
+            } else return UserInfoResponse.builder()
+                    .kakaoUserInfoResponse(kakaoUserInfoResponse)
+                    .isSignedUp(false)
+                    .build();
         } catch (JSONException e) {
             throw new RuntimeException(e);
         }
--- src/main/java/com/kkm/kkm_server_v2/global/infra/kakao/KakaoLoginClient.java
+++ src/main/java/com/kkm/kkm_server_v2/global/infra/kakao/KakaoLoginClient.java
@@ -1,12 +1,8 @@
 package com.kkm.kkm_server_v2.global.infra.kakao;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.kkm.kkm_server_v2.domain.auth.presentation.dto.response.KakaoUserInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "oauth-token-client", url = "https://kauth.kakao.com")
