dcda89ad0135e4416c5a94343c2e1f3f3fcafab2
yongbin1 <rhdydqls1024@naver.com>
2023-05-16 23:32:47 +0900

    바보

--- src/main/java/com/kkm/kkm_server_v2/global/security/auth/AuthDetailsService.java
+++ src/main/java/com/kkm/kkm_server_v2/global/security/auth/AuthDetailsService.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +16,7 @@ public class AuthDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
     @Override
+    @Transactional
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUserId(username)
                 .map(AuthDetails::new)
