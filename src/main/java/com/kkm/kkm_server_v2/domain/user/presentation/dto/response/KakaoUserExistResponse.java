88b3e0f42c2b7fe4e0e5f3f651376158fc8a9788
soohyuneeee <soohyuneeee12@gmail.com>
2023-05-30 11:25:42 +0900

    Fix Response Dto

--- src/main/java/com/kkm/kkm_server_v2/domain/user/presentation/dto/response/KakaoUserExistResponse.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/user/presentation/dto/response/KakaoUserExistResponse.java
@@ -1,7 +1,9 @@
 package com.kkm.kkm_server_v2.domain.user.presentation.dto.response;
 
 import lombok.Builder;
+import lombok.Getter;
 
+@Getter
 @Builder
 public class KakaoUserExistResponse {
     private boolean isExist;
