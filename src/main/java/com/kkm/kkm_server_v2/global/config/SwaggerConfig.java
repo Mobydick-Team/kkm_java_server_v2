0e0f0fa5bddb82d091425a02abd1299c6bde0ed8
yongbin1 <rhdydqls1024@naver.com>
2023-05-30 23:43:34 +0900

    Swagger Token 사용 가능

--- src/main/java/com/kkm/kkm_server_v2/global/config/SwaggerConfig.java
+++ src/main/java/com/kkm/kkm_server_v2/global/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.kkm.kkm_server_v2.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +18,12 @@ public class SwaggerConfig {
                 .description("꼬꼬막 API 문서");
 
         return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(
+                                "bearer-token",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer").bearerFormat("JWT")))
                 .info(info);
     }
 
