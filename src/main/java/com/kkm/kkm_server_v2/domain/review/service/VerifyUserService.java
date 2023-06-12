0f711813d15cb23365b2e1c19a10823fc3113033
soohyuneeee <soohyuneeee12@gmail.com>
2023-06-13 08:10:14 +0900

    {Add} sout

--- src/main/java/com/kkm/kkm_server_v2/domain/review/service/VerifyUserService.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/review/service/VerifyUserService.java
@@ -8,7 +8,9 @@ import com.kkm.kkm_server_v2.domain.trade.domain.Trade;
 import com.kkm.kkm_server_v2.domain.trade.domain.repository.TradeRepository;
 import com.kkm.kkm_server_v2.domain.trade.exception.TradeNotCompletedException;
 import com.kkm.kkm_server_v2.domain.user.domain.User;
+import com.sun.istack.logging.Logger;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +28,8 @@ public class VerifyUserService {
                 .orElseThrow(() -> PostNotFoundException.EXCEPTION);
         Trade trade = tradeRepository.findByPostIdAndReceiverId(post, user.getId());
         if (trade == null) throw ReviewAccessWrongException.EXCEPTION;
+        System.out.println("trade : "+ trade.getTradeTime());
+        System.out.println("now : "+ LocalDateTime.now());
         if (trade.getTradeTime().isBefore(LocalDateTime.now())) throw TradeNotCompletedException.EXCEPTION;
         return trade;
     }
