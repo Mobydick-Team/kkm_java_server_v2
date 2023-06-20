0aa92e0b2de4999960c0f44b3e54bd4fee27b0ee
soohyuneeee <soohyuneeee12@gmail.com>
2023-06-20 15:25:33 +0900

    {Add} LocationSelectedCannotDeleteException

--- src/main/java/com/kkm/kkm_server_v2/domain/location/exception/error/LocationErrorProperty.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/location/exception/error/LocationErrorProperty.java
@@ -11,7 +11,8 @@ public enum LocationErrorProperty implements ErrorProperty {
     LOCATION_COUNT_EXCEED(HttpStatus.BAD_REQUEST, "위치는 3개까지 등록할 수 있습니다."),
     LOCATION_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 등록된 위치 정보입니다."),
     LOCATION_NOT_FOUND(HttpStatus.NOT_FOUND, "위치 정보를 찾을 수 없습니다."),
-    LOCATION_ALREADY_SELECTED(HttpStatus.BAD_REQUEST, "이미 선택된 위치입니다.");
+    LOCATION_ALREADY_SELECTED(HttpStatus.BAD_REQUEST, "이미 선택된 위치입니다."),
+    LOCATION_SELECTED_CANNOT_DELETE(HttpStatus.BAD_REQUEST, "선택된 주소는 삭제할 수 없습니다.");
 
     private final HttpStatus status;
     private final String message;
