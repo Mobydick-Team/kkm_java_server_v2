d0a1479c8ce8a4dfe5b95cfa919cbf5b530089b3
soohyuneeee <soohyuneeee12@gmail.com>
2023-06-20 13:52:32 +0900

    {Fix} - 안쓰는 클래스 정리

--- src/main/java/com/kkm/kkm_server_v2/domain/post/service/PullPostService.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/post/service/PullPostService.java
@@ -1,7 +1,6 @@
 package com.kkm.kkm_server_v2.domain.post.service;
 
 import com.kkm.kkm_server_v2.domain.post.domain.Post;
-import com.kkm.kkm_server_v2.domain.post.domain.repository.PostRepository;
 import com.kkm.kkm_server_v2.domain.post.facade.PostFacade;
 import com.kkm.kkm_server_v2.domain.user.domain.User;
 import com.kkm.kkm_server_v2.domain.user.facade.UserFacade;
@@ -9,15 +8,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDateTime;
-
 @Service
 @RequiredArgsConstructor
 public class PullPostService {
 
     private final UserFacade userFacade;
     private final PostFacade postFacade;
-    private final PostRepository postRepository;
 
     @Transactional
     public void execute(Long postId) {
