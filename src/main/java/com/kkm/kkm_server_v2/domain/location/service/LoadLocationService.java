49c5aa329c07fb245a389bdacc4b09036b598bea
soohyuneeee <soohyuneeee12@gmail.com>
2023-06-20 11:15:58 +0900

    {Add} - LoadLocationService

---
+++ src/main/java/com/kkm/kkm_server_v2/domain/location/service/LoadLocationService.java
@@ -0,0 +1,28 @@
+package com.kkm.kkm_server_v2.domain.location.service;
+
+import com.kkm.kkm_server_v2.domain.location.domain.Location;
+import com.kkm.kkm_server_v2.domain.location.domain.repository.LocationRepository;
+import com.kkm.kkm_server_v2.domain.location.presentation.dto.response.LoadLocationListResponse;
+import com.kkm.kkm_server_v2.domain.location.presentation.dto.response.LoadLocationResponse;
+import com.kkm.kkm_server_v2.domain.user.domain.User;
+import com.kkm.kkm_server_v2.domain.user.facade.UserFacade;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class LoadLocationService {
+    private final UserFacade userFacade;
+    private final LocationRepository locationRepository;
+
+    public LoadLocationListResponse execute() {
+        User user = userFacade.getCurrentUser(false);
+        List<Location> locationList = locationRepository.findAllByUserLocation(user);
+        return LoadLocationListResponse.builder()
+                .locationList(locationList.stream().map(LoadLocationResponse::of).collect(Collectors.toList()))
+                .build();
+    }
+}
