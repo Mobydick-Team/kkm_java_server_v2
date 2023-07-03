33bada19ab26f729ba75b08ef1f01602fb85cf1d
soohyuneeee <soohyuneeee12@gmail.com>
2023-07-03 22:05:16 +0900

    {Fix} url 변경

--- src/main/java/com/kkm/kkm_server_v2/domain/auth/presentation/AuthController.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/auth/presentation/AuthController.java
@@ -1,14 +1,23 @@
 package com.kkm.kkm_server_v2.domain.auth.presentation;
 
+import com.kkm.kkm_server_v2.domain.auth.presentation.dto.request.LoginRequest;
 import com.kkm.kkm_server_v2.domain.auth.presentation.dto.response.AccessTokenResponse;
+import com.kkm.kkm_server_v2.domain.auth.presentation.dto.response.TokenResponse;
+import com.kkm.kkm_server_v2.domain.auth.service.KakaoUserExistService;
+import com.kkm.kkm_server_v2.domain.auth.service.LoginService;
+import com.kkm.kkm_server_v2.domain.auth.service.LogoutService;
 import com.kkm.kkm_server_v2.domain.auth.service.RefreshTokenService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/auth")
@@ -16,10 +25,27 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "인증 서버")
 public class AuthController {
     private final RefreshTokenService refreshTokenService;
+    private final LoginService loginService;
+    private final KakaoUserExistService kakaoUserExistService;
+    private final LogoutService logoutService;
+
 
     @Operation(summary = "토큰 재발급")
     @PutMapping
     public AccessTokenResponse refreshToken(@RequestHeader(value = "Refresh-Token") String refreshToken) {
         return refreshTokenService.execute(refreshToken);
+    }
+
+    @Operation(summary = "로그인")
+    @PostMapping("/login")
+    public TokenResponse login(@RequestBody @Valid LoginRequest request) {
+        kakaoUserExistService.execute(request.getUserId());
+        return loginService.execute(request);
+    }
+
+    @Operation(summary = "로그아웃")
+    @PostMapping("/logout")
+    public void logout() {
+        logoutService.execute();
     }
 }
