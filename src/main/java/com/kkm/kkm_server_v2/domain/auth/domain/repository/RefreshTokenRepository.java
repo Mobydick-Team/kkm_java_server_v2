7843d702606ab2e0189a230de552da59f8dcd0ab
soohyuneeee <soohyuneeee12@gmail.com>
2023-07-03 22:04:27 +0900

    {Add} LogoutService

--- src/main/java/com/kkm/kkm_server_v2/domain/auth/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/kkm/kkm_server_v2/domain/auth/domain/repository/RefreshTokenRepository.java
@@ -4,6 +4,9 @@ import com.kkm.kkm_server_v2.domain.auth.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+    Optional<RefreshToken> findByUserId(String userId);
 }
